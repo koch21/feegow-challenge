33fdeb1ebfc59b6ac51ad4f6eff54e86cd3520f4
koch21 <hagnakoch@gmail.com>
2021-07-24 06:58:09 -0300

    estiliza & documenta

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container } from "./styles";
 
 import Header from "../../components/header";
-import ProfessionalList from "../../components/professionalList";
 import DoctorList from "../../components/doctorList";
 
 const Home = () => {
