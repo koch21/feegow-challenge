327e1e8989b3925c2bf32b2993e8befd06bf15f3
koch21 <hagnakoch@gmail.com>
2023-06-28 19:58:37 -0300

    removendo Valores do firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDsd1NDI4r9IdJKJ4XKy_2Sr9ZmBV4fnJI",
-  authDomain: "feegow-test-a44be.firebaseapp.com",
-  projectId: "feegow-test-a44be",
-  storageBucket: "feegow-test-a44be.appspot.com",
-  messagingSenderId: "800135016954",
-  appId: "1:800135016954:web:ccb0f3b8688a782af27443",
-  measurementId: "G-LNYN5RF6L0",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 });
 
 export const auth = app.auth();
